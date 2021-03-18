d74fdf3c222cb91cfa1035886dfb5ae5ebbb7f6b
supriyayadav221 <supriyayadav221@gmail.com>
2021-03-18 15:30:54 +0530

    comments

--- test.c
+++ test.c
@@ -1,7 +1,19 @@
+/**
+ * @file test.c
+ * @author Supriya_265163
+ * @brief 
+ * @version 0.1
+ * @date 2021-03-18
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
 #include <stdio.h>
 int main()
 {
     printf("LTTS");
+    printf("Candidate: Supriya");
+
 
     return 0;
 }
