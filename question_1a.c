9418000f13a7ce31bb01c01ad2a786c2788d3088
supriyayadav221 <supriyayadav221@gmail.com>
2021-03-22 21:14:05 +0530

    multiply

--- question_1a.c
+++ question_1a.c
@@ -24,8 +24,14 @@ n2.real=56;
 n2.imaginary=78;
 
 add_result=add(n1,n2);
-printf("Sum = %.1f + %.1fi", add_result.real, add_result.imaginary);
+multiplication_result = multiply(n1,n2);
+subtract_result=subtract(n1,n2);
 
+printf("Multiply = %.1f * %.1fi\n", multiplication_result.real, multiplication_result.imaginary);
+
+printf("Sum = %.1f + %.1fi\n", add_result.real, add_result.imaginary);
+
+printf("Subtract = %.1f - %.1fi\n", subtract_result.real, subtract_result.imaginary);
 
 
 }
@@ -35,5 +41,21 @@ complex_t add(complex_t a, complex_t b)
     complex_t temp;
     temp.real = a.real + b.real;
     temp.imaginary=a.imaginary+ b.imaginary;
+    return temp;
+}
+
+complex_t multiply(complex_t a, complex_t b)
+{
+    complex_t temp;
+    temp.real = a.real * b.real;
+    temp.imaginary=a.imaginary* b.imaginary;
+    return temp;
+}
+
+complex_t subtract(complex_t a, complex_t b)
+{
+    complex_t temp;
+    temp.real = a.real - b.real;
+    temp.imaginary=a.imaginary - b.imaginary;
     return temp;
 }
