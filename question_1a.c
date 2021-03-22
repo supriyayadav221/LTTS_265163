d67c4c3a79898516c27f5984c3e7a93225a2da6b
supriyayadav221 <supriyayadav221@gmail.com>
2021-03-22 21:35:12 +0530

    rectified multiply

--- question_1a.c
+++ question_1a.c
@@ -27,13 +27,13 @@ add_result=add(n1,n2);
 multiplication_result = multiply(n1,n2);
 subtract_result=subtract(n1,n2);
 
-printf("Multiply = %.1f * %.1fi\n", multiplication_result.real, multiplication_result.imaginary);
+printf("Multiply = %.1f + %.1f\n", multiplication_result.real, multiplication_result.imaginary);
 
 printf("Sum = %.1f + %.1fi\n", add_result.real, add_result.imaginary);
 
-printf("Subtract = %.1f - %.1fi\n", subtract_result.real, subtract_result.imaginary);
+printf("Subtract = %.1f  %.1fi\n", subtract_result.real, subtract_result.imaginary);
 
-
+printf("Division = %.1f / %.1fi\n", division_result.real, division_result.imaginary);
 }
 
 complex_t add(complex_t a, complex_t b)
@@ -47,8 +47,8 @@ complex_t add(complex_t a, complex_t b)
 complex_t multiply(complex_t a, complex_t b)
 {
     complex_t temp;
-    temp.real = a.real * b.real;
-    temp.imaginary=a.imaginary* b.imaginary;
+   temp.real = a.real*b.real - a.imaginary*b.imaginary;
+     temp.imaginary = a.imaginary*b.real + a.real*b.imaginary;
     return temp;
 }
 
@@ -57,5 +57,13 @@ complex_t subtract(complex_t a, complex_t b)
     complex_t temp;
     temp.real = a.real - b.real;
     temp.imaginary=a.imaginary - b.imaginary;
+    return temp;
+}
+
+complex_t division(complex_t a, complex_t b)
+{
+    complex_t temp;
+    temp.real = a.real / b.real;
+    temp.imaginary=a.imaginary / b.imaginary;
     return temp;
 }
